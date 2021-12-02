fd3d671833c9b5fcc7c4868bddb322c79e31ed81
Marcelo Campos <40444189+marcelo85@users.noreply.github.com>
2021-12-01 21:03:26 -0300

    Add files via upload

---
+++ Teste Final/src/teste/App.java
@@ -0,0 +1,63 @@
+package teste;
+
+import javax.swing.JOptionPane;
+
+public class App {
+    static Turma classe = new Turma();
+    
+    public static int menu() {
+        String menu = "MENU\n";
+        menu = menu + "1 – Cadastrar Aluno\n";
+        menu = menu + "2 – Buscar Aluno\n";
+        menu = menu + "3 – Sair";
+        int opcao = 0;
+        String aux = JOptionPane.showInputDialog(menu);
+        opcao = Integer.valueOf(aux);
+        return opcao;
+    }
+    public static void cadastrarAluno(){
+        
+        String nome,matricula;
+        nome=   JOptionPane.showInputDialog(null,
+                "Qual o nome do Aluno?",
+                "Controle de Aluno - Informar Nome",
+                JOptionPane.QUESTION_MESSAGE);
+        matricula=   JOptionPane.showInputDialog(null,
+                "Qual a matricula do Aluno?",
+                "Controle de Aluno - Informar Matricula",
+                JOptionPane.QUESTION_MESSAGE);
+        classe.cadastrarAluno(matricula,nome); 
+        JOptionPane.showMessageDialog(null,"Cadastrado com sucesso!");   
+    }
+    
+    public static void buscarAluno(){
+        
+        String matricula;
+        matricula=   JOptionPane.showInputDialog(null,
+                "Qual a matricula do Aluno?",
+                "Controle de Aluno - Buscar Aluno",
+                JOptionPane.QUESTION_MESSAGE);
+        JOptionPane.showMessageDialog(null,classe.buscarAluno(matricula));
+    }
+    public static void main(String[] args) {
+         
+    int op;
+    do {
+            op = menu();
+            switch (op) {
+                case 1:
+                    cadastrarAluno();
+                break;
+
+                case 2:
+                    buscarAluno();
+                break;
+
+                case 3: 
+                     System.exit(0); 
+                break;
+
+            }
+        } while (op >= 1 && op <= 3);
+    }   
+}
---
+++ Teste Final/src/teste/Turma.java
@@ -0,0 +1,18 @@
+package teste;
+
+public class Turma {
+	private Aluno turma[]=new Aluno[30];
+    private int tam=0;
+    
+    public void cadastrarAluno(String matricula,String nome){
+        turma[tam]= new Aluno(matricula,nome);
+        tam++;
+    }
+    public Aluno buscarAluno(String matricula){
+        for(int i=0 ; i<tam ;i++){
+            if(turma[i].getMatricula().equals(matricula))
+                return turma[i];
+        }
+        return null;
+    } 
+}
